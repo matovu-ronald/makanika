ccc9c228de86e81a5922bf7f9b1c0c4a8c1b9623
Matovu Ronald <maronwhite6@gmail.com>
2023-12-10 18:55:50 +0300

    Add new images and update Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.html"],
+  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "makanika-cherry-red": "#F80404",
+        "makanika-pastel-pink": "#FFF1F173",
+        "makanika-sky-blue": "#D2D6FD",
+        "makanika-ice-blue": "#D2E5F6",
+        "makanika-sage-green": "#205B28",
+        "makanika-butter-yellow": "#FBE2A6",
+        "makanika-azure-blue": "#C5F5FF",
+        "makanika-blush-pink": "#FFF8F8",
+        "makanika-ghost-gray": "#F8FEFF",
+        "makanika-apple-green": "#E2FDD2",
+        "makanika-frost-blue": "#EAF5FF",
+        "makankika-jet-black": "#181716",
+        "makanika-sunflower-yellow": "#EEAF19",
+      },
+      gridTemplateColumns: {
+        "full-bleed": "1fr repeat(2, minmax(auto, 39em)) 1fr;",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
